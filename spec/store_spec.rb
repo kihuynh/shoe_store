71976f8fc1baec25b412382ea15894d4c89244d1
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-03-02 11:57:00 -0800

    fail first spec for Store

--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
-describe('Stores') do
+describe(Store) do
+  it("validates there's a name for the store")
+  store = Store.new({:name => ""})
+  expect(store.save).to(eq(false))
 
 end
