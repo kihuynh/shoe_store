7869522e4830ee3f1ec39c1f40e31d6a6b4fa655
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-03-02 12:23:11 -0800

    fail spec for validating max 100 characters for store_spec

--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -3,8 +3,13 @@ require 'spec_helper'
 describe(Store) do
   # it { should have_and_belongs_to_many(:brands)}
 
-  it("validates there's a name for the store") do
+  it('validates there is a name for the store') do
     store = Store.new({:name => ""})
+    expect(store.save).to(eq(false))
+  end
+
+  it('validates length of name is at most 100 characters') do
+    store = Store.new({:name => "a".*(101)})
     expect(store.save).to(eq(false))
   end
 end
