2c5163b263b6bf4bef66cc5fc056f7ab34439af6
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-03-03 12:59:52 -0800

    pass spec to titleize shoe brand names

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,7 +1,10 @@
 class Brand < ActiveRecord::Base
   has_and_belongs_to_many(:stores)
   # validates(:name, :presence => true)
+  before_save(:upcase_name)
 
   private
-
+  def upcase_name
+    self.name = self.name.titleize
+  end
 end
--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -3,9 +3,13 @@ require('spec_helper')
 describe(Brand) do
     it { should have_and_belong_to_many(:stores)}
 
+    # it('validates there is a name for the brand') do
+    #   brand = Store.new({:name => ""})
+    #   expect(brand.save).to eq false
+    # end
     it('saves shoe brand input as title cases') do
       brand = Brand.new({:name => "sam edelman"})
       brand.save
-      expect(brand.name).to eq "Same Edelman"
+      expect(brand.name).to eq "Sam Edelman"
     end
 end
