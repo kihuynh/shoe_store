c45eaeb6695b9ad3ce6860070e61d7f285728bdc
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-03-02 22:55:08 -0800

    not use form buttons to navigate to other pages. will break things. switch to button nest in an a tag

--- app.rb
+++ app.rb
@@ -15,19 +15,19 @@ get('/') do
   erb(:home)
 end
 
+# gets all the stores
+get('/stores') do
+  @stores = Store.all
+  erb(:store)
+end
 # click on add store button and route here
 # enter store name. click add and redirect to go back home
 post('/stores') do
   store_name = params.fetch('store_name')
   @store = Store.new({:name => store_name})
-  # @store.save
-  # @stores = Store.all
-
-  erb(:store)
-end
-# gets all the stores
-get('/stores') do
+  @store.save
   @stores = Store.all
+
   erb(:store)
 end
 # with this get it stores it. but when you initially hit the add a store button. it goes to the key error page. BUT if you refresh the page using the URL bar and not hitting refresh, then it does show up properly and add the store to the DB.
