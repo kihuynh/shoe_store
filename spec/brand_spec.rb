016b323b353d506e74f390334a621ea0c77d7402
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-03-03 12:36:27 -0800

    fail spec to titlize shoe brand names

--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -2,4 +2,10 @@ require('spec_helper')
 
 describe(Brand) do
     it { should have_and_belong_to_many(:stores)}
+
+    it('saves shoe brand input as title cases') do
+      brand = Brand.new({:name => "sam edelman"})
+      brand.save
+      expect(brand.name).to eq "Same Edelman"
+    end
 end
