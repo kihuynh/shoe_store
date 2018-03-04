fcf95b0de692379375830917098ef8c584e26294
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-03-04 13:04:29 -0800

    clean up app and view files. add app.rb duplicate with notes

--- app.rb
+++ app.rb
@@ -7,37 +7,31 @@ require('./lib/brand')
 require('pry')
 require('pg')
 
-# home base //reads all brands and stores
-# // link to add individual shoe stores and brands
 get('/') do
   @brands = Brand.all
   @stores = Store.all
   erb(:home)
 end
 
-# gets all the stores
 get('/stores') do
   @stores = Store.all
   erb(:store)
 end
-# click on add store button and route here
-# enter store name. click add and redirect to go back home
+
 post('/stores') do
   store_name = params.fetch('store_name')
   @store = Store.new({:name => store_name})
   @store.save
   @stores = Store.all
-
   erb(:store)
 end
-# go to individual store / display shoes
+
 get('/stores/:id') do
   @store = Store.find(params.fetch("id"))
   @stores = Store.all
   erb(:stores)
 end
 
-# update individual store
 patch('/stores/:id') do
   @store = Store.find(params.fetch("id").to_i)
   store_name = params.fetch('store_name')
@@ -46,23 +40,19 @@ patch('/stores/:id') do
   erb(:stores)
 end
 
-# delete individual store
-  delete('/stores/:id') do
-    @store = Store.find(params.fetch("id").to_i)
-    @store.delete
-    @stores = Store.all
-    erb(:store)
-  end
+delete('/stores/:id') do
+  @store = Store.find(params.fetch("id").to_i)
+  @store.delete
+  @stores = Store.all
+  erb(:store)
+end
 
 # Shoes
-# gets all brands to list out
-
 get('/brands') do
   @brands = Brand.all
   erb(:brand)
 end
 
-# add shoe brands to list
 post('/brands') do
   brand_name = params.fetch('brand_name')
   price = params.fetch("price")
@@ -71,43 +61,41 @@ post('/brands') do
   @brands = Brand.all
   erb(:brand)
 end
-# look at individual shoe brands
+
 get('/brands/:id') do
   @brand = Brand.find(params.fetch("id").to_i)
   @brands = Brand.all
   @stores = Store.all
   erb(:brands)
 end
-#  add shoes to store
-  patch('/brands/:id') do
-    @store = Store.find(params.fetch("store_id").to_i)
-    @brand = Brand.find(params.fetch("id").to_i)
-    @brand.stores.push(store)
-    @brands = Brand.all
-    @stores = Store.all
-    erb(:brands)
-  end
 
-# MOVE EDIT OPTIONS
-# move edit to here
-  get('/brands/:id/edit') do
-    @brand = Brand.find(params.fetch("id").to_i)
-    @brands = Brand.all
-    @stores = Store.all
-    erb(:brand_edit)
-  end
+patch('/brands/:id') do
+  @store = Store.find(params.fetch("store_id").to_i)
+  @brand = Brand.find(params.fetch("id").to_i)
+  @brand.stores.push(store)
+  @brands = Brand.all
+  @stores = Store.all
+  erb(:brands)
+end
 
-  patch('/brands/:id/edit')do
-    @brand = Brand.find(params.fetch("id").to_i)
-    brand_name = params.fetch('brand_name')
-    @brand.update({:name => brand_name})
-    erb(:brand_edit)
-  end
-  # delete individual brand
-  delete('/brands/:id/edit') do
-    @brand = Brand.find(params.fetch("id").to_i)
-    @brand.delete
-    @brands = Brand.all
-    @stores = Store.all
-    erb(:brands)
-  end
+get('/brands/:id/edit') do
+  @brand = Brand.find(params.fetch("id").to_i)
+  @brands = Brand.all
+  @stores = Store.all
+  erb(:brand_edit)
+end
+
+patch('/brands/:id/edit')do
+  @brand = Brand.find(params.fetch("id").to_i)
+  brand_name = params.fetch('brand_name')
+  @brand.update({:name => brand_name})
+  erb(:brand_edit)
+end
+
+delete('/brands/:id/edit') do
+  @brand = Brand.find(params.fetch("id").to_i)
+  @brand.delete
+  @brands = Brand.all
+  @stores = Store.all
+  erb(:brands)
+end
---
+++ misc.rb
@@ -0,0 +1,105 @@
+#duplicate of App.rb but with notes
+# home base //reads all brands and stores
+# // link to add individual shoe stores and brands
+get('/') do
+  @brands = Brand.all
+  @stores = Store.all
+  erb(:home)
+end
+
+# gets all the stores
+get('/stores') do
+  @stores = Store.all
+  erb(:store)
+end
+# click on add store button and route here
+# enter store name. click add and redirect to go back home
+post('/stores') do
+  store_name = params.fetch('store_name')
+  @store = Store.new({:name => store_name})
+  @store.save
+  @stores = Store.all
+
+  erb(:store)
+end
+# go to individual store / display shoes
+get('/stores/:id') do
+  @store = Store.find(params.fetch("id"))
+  @stores = Store.all
+  erb(:stores)
+end
+
+# update individual store
+patch('/stores/:id') do
+  @store = Store.find(params.fetch("id").to_i)
+  store_name = params.fetch('store_name')
+  @store.update({:name => store_name})
+  @stores = Store.all
+  erb(:stores)
+end
+
+# delete individual store
+  delete('/stores/:id') do
+    @store = Store.find(params.fetch("id").to_i)
+    @store.delete
+    @stores = Store.all
+    erb(:store)
+  end
+
+# Shoes
+# gets all brands to list out
+
+get('/brands') do
+  @brands = Brand.all
+  erb(:brand)
+end
+
+# add shoe brands to list
+post('/brands') do
+  brand_name = params.fetch('brand_name')
+  price = params.fetch("price")
+  @brand = Brand.new({:name => brand_name, :price => price})
+  @brand.save
+  @brands = Brand.all
+  erb(:brand)
+end
+# look at individual shoe brands
+get('/brands/:id') do
+  @brand = Brand.find(params.fetch("id").to_i)
+  @brands = Brand.all
+  @stores = Store.all
+  erb(:brands)
+end
+#  add shoes to store
+  patch('/brands/:id') do
+    @store = Store.find(params.fetch("store_id").to_i)
+    @brand = Brand.find(params.fetch("id").to_i)
+    @brand.stores.push(store)
+    @brands = Brand.all
+    @stores = Store.all
+    erb(:brands)
+  end
+
+# Brand Editing
+# move edit to here
+  get('/brands/:id/edit') do
+    @brand = Brand.find(params.fetch("id").to_i)
+    @brands = Brand.all
+    @stores = Store.all
+    erb(:brand_edit)
+  end
+
+  patch('/brands/:id/edit')do
+    @brand = Brand.find(params.fetch("id").to_i)
+    brand_name = params.fetch('brand_name')
+    @brand.update({:name => brand_name})
+    erb(:brand_edit)
+  end
+  # delete individual brand
+  delete('/brands/:id/edit') do
+    @brand = Brand.find(params.fetch("id").to_i)
+    @brand.delete
+    @brands = Brand.all
+    @stores = Store.all
+    erb(:brands)
+  end
